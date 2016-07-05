fa19122dae277c9f1ce564fba273c699600338fb
marcel <marcel@marcels-mbp.br-edv.brnet.int>
2016-07-05 15:02:05 +0200

    set webpack chunks output in command line to false

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
 module.exports = {
-  entry: "./app/App.js",
-  output: {
-    filename: "public/bundle.js"
-  },
-  devServer: {
-  	inline: true,
-  	port: 3000
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
-  }
+	entry: "./app/App.js",  
+	output: {
+	filename: "public/bundle.js"
+},
+devServer: {
+	inline: true,
+	port: 3000,
+	stats: {
+		chunks: false
+	}
+},
+module: {
+	loaders: 
+		[
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel',
+				query: {
+					presets: ['react', 'es2015']
+				}
+		 	}
+		]
+	}
 }
